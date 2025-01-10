60bf794c924bc729d9d9255c96824f11f456f531
Jatin <ym.jatin@gmail.com>
2025-01-10 14:20:26 +0530

    wip: base routing setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { GlobalErrorLayout, Layout, NotFound } from './components';
+import './index.css';
+import { Home, Pokemon } from './routes';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<App />} />
+                <Route path="/" errorElement={<GlobalErrorLayout />}>
+                    <Route index element={<Home />} />
+                    <Route element={<Layout />}>
+                        <Route path="pokemon" element={<Pokemon />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     </StrictMode>
