6f7d29a4ca53ecdbf38bac8e64dd16ce19326d80
Jatin <ym.jatin@gmail.com>
2025-01-10 14:18:56 +0530

    wip: not found comp and other comp exports

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export * from './footer';
+export * from './header';
+export * from './layouts';
+export * from './not-found';
---
+++ src/components/not-found/not-found.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export type NotFoundProps = {};
+
+export const NotFound: FC<NotFoundProps> = () => {
+    return <>404 | Not Found</>;
+};
