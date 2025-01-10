dd96591124b94f092b1b7a00c8ae242ae9a88719
Jatin <ym.jatin@gmail.com>
2025-01-10 14:13:15 +0530

    wip: header, footer comp

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export type FooterProps = {};
+
+export const Footer: FC<FooterProps> = () => {
+    return <div>Footer</div>;
+};
---
+++ src/components/header/header.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export type HeaderProps = {};
+
+export const Header: FC<HeaderProps> = () => {
+    return <div>Header</div>;
+};
