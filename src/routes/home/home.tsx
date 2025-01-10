ab3aecf9b054d5c04b0798ae036d3a2ebee734ac
Jatin <ym.jatin@gmail.com>
2025-01-10 14:18:26 +0530

    wip: home,pokemon route

---
+++ src/routes/home/home.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export type HomeProps = {};
+
+export const Home: FC<HomeProps> = () => {
+    return <>welcome to pookiémon</>;
+};
---
+++ src/routes/pokemon/pokemon.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export type PokemonProps = {};
+
+export const Pokemon: FC<PokemonProps> = () => {
+    return <>Pokemon</>;
+};
