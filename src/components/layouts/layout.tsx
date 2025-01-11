49df5734e511033f678e37fc3262b890d0cca91c
Jatin <ym.jatin@gmail.com>
2025-01-11 14:03:09 +0530

    wip: add container in layout comp

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { Outlet } from 'react-router';
 import { Footer } from '../footer/footer';
 import { Header } from '../header/header';
+import { Container } from '@styled-system/jsx';
 
 export type LayoutProps = {};
 
@@ -9,7 +10,14 @@ export const Layout: FC<LayoutProps> = () => {
     return (
         <>
             <Header />
-            <Outlet />
+            <Container
+                maxW={'breakpoint-2xl'}
+                minH={'screen'}
+                display={'flex'}
+                justifyContent={'center'}
+            >
+                <Outlet />
+            </Container>
             <Footer />
         </>
     );
