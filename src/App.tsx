86f8f60472e36b8a7d70345f885f4b73c3ac5770
Jatin <ym.jatin@gmail.com>
2025-01-23 14:39:07 +0530

    wip: routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
-import { GlobalErrorLayout, Layout, NotFound } from './components';
-import { Berries, Home, Items, Pokemon } from './routes';
+import { GlobalErrorLayout, Layout, NotFound, ScrollToTop } from './components';
+import { Berries, Berry, Home, Item, Items, Pokemon, Pokemons } from './routes';
 
 export default function App() {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Routes>
                 <Route path="/" errorElement={<GlobalErrorLayout />}>
                     <Route index element={<Home />} />
                     <Route element={<Layout />}>
-                        <Route path="pokemon" element={<Pokemon />} />
+                        <Route path="pokemon" element={<Pokemons />} />
+                        <Route path="pokemon/:nameOrId" element={<Pokemon />} />
                         <Route path="berries" element={<Berries />} />
+                        <Route path="berries/:nameOrId" element={<Berry />} />
                         <Route path="items" element={<Items />} />
+                        <Route path="items/:nameOrId" element={<Item />} />
                     </Route>
                     <Route path="*" element={<NotFound />} />
                 </Route>
