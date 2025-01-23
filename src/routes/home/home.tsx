dc90061a22a77c1ce1c4eb7f6386a02128ff40fd
Jatin <ym.jatin@gmail.com>
2025-01-23 14:34:00 +0530

    wip: home page

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,7 +1,10 @@
+import { BodyText } from '@/components';
 import { css } from '@styled-system/css';
 import { Container, Flex } from '@styled-system/jsx';
 import { FC } from 'react';
+import { HiArrowUpRight } from 'react-icons/hi2';
 import { Link } from 'react-router';
+import homebg from '@/assets/home-bg.jpg';
 
 export type HomeProps = {};
 
@@ -13,24 +16,75 @@ export const Home: FC<HomeProps> = () => {
             display={'flex'}
             justifyContent={'center'}
             alignItems={'center'}
+            flexDir={'column'}
         >
-            <Flex
-                direction={'column'}
-                alignItems={'center'}
-                justifyContent={'center'}
-                h={'full'}
-                gap={'3'}
-            >
-                <div className={css({ textAlign: 'center', mb: '1' })}>
-                    <h1 className={css({ fontSize: '3xl' })}>welcome to pookiémon</h1>
-                    <p className={css({ fontSize: 'sm' })}>explore the world the of pokémon</p>
-                </div>
-                <Flex gap={'5'} alignItems={'center'} justifyContent={'center'}>
-                    <Link to={'/pokemon'}>pokémon</Link>
-                    <Link to={'/berries'}>berries</Link>
-                    <Link to={'/items'}>items</Link>
-                </Flex>
+            <BgImg imgSrc={homebg} />
+            <Flex direction={'column'} className={css({ textAlign: 'center', mb: '1' })}>
+                <h1
+                    className={css({
+                        fontSize: '4xl',
+                        md: {
+                            fontSize: '6xl'
+                        },
+                        fontFamily: 'logo',
+                        lineHeight: 1.1
+                    })}
+                >
+                    <span className={css({ color: 'pink.300' })}>pookié</span>mon
+                </h1>
+                <BodyText>explore the world the of pokémon</BodyText>
+            </Flex>
+            <Flex gap={'8'} alignItems={'center'} justifyContent={'center'} mt={'6'}>
+                <Link
+                    to={'/pokemon'}
+                    className={css({
+                        transition: 'color .3s ease-in-out',
+                        _hover: {
+                            color: 'pink.300'
+                        }
+                    })}
+                >
+                    <Flex alignItems={'center'} gap={1}>
+                        <BodyText>pokémon</BodyText>
+                        <HiArrowUpRight strokeWidth={1} />
+                    </Flex>
+                </Link>
+                <Link to={'/berries'}>
+                    <Flex alignItems={'center'} gap={1}>
+                        <BodyText>berries</BodyText>
+                        <HiArrowUpRight strokeWidth={1} />
+                    </Flex>
+                </Link>
+                <Link to={'/items'}>
+                    <Flex alignItems={'center'} gap={1}>
+                        <BodyText>items</BodyText>
+                        <HiArrowUpRight strokeWidth={1} />
+                    </Flex>
+                </Link>
             </Flex>
         </Container>
+    );
+};
+
+interface BgImgProps {
+    imgSrc: string;
+}
+const BgImg: FC<BgImgProps> = ({ imgSrc }) => {
+    return (
+        <img
+            className={css({
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: 'screen',
+                height: 'screen',
+                objectFit: 'cover',
+                opacity: '15%',
+                zIndex: -1
+            })}
+            src={imgSrc}
+            alt="bg image"
+            loading="eager"
+        />
     );
 };
