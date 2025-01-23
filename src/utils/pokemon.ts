b49dbb1b15b2ef8206fa0f20818fae5be016dfae
Jatin <ym.jatin@gmail.com>
2025-01-23 14:28:17 +0530

    chore: util functions to fetch resource sprites

--- src/utils/pokemon.ts
+++ src/utils/pokemon.ts
@@ -1,4 +1,23 @@
-export const getPokemonIdFromUrl = (url: string): number => {
+export const getResourceIdFromUrl = (url: string): number => {
     const id = Number(url.split('/')[url.split('/').length - 2]);
     return id;
+};
+
+const baseSpritePath = `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/`;
+
+export const getResourceDefaultSprite = (
+    resourceType: 'pokemon' | 'item' | 'berry',
+    name: string,
+    url: string
+) => {
+    if (resourceType === 'pokemon') {
+        const spriteSrc = `${baseSpritePath}pokemon/${getResourceIdFromUrl(url)}.png`;
+        const spriteOfficialArtworkSrc = `${baseSpritePath}pokemon/other/official-artwork/${getResourceIdFromUrl(url)}.png`;
+        const gifSrc = `${baseSpritePath}pokemon/other/showdown/${getResourceIdFromUrl(url)}.gif`;
+        return spriteOfficialArtworkSrc;
+    } else if (resourceType === 'berry') {
+        return `${baseSpritePath}items/${name}-berry.png`;
+    } else {
+        return `${baseSpritePath}items/${name}.png`;
+    }
 };
